6481facbbd4f6a320d0fc66276ce9711715de74e
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-07-06 10:09:48 +0530

    slider update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,79 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu_1").classList.toggle("cross")
 }
+$(document).ready(function () {
+    $(".parentslider").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+        ]
+    });
+    $('.parent-rtl').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrow: false,
+        dots: false,
+        rtl: true,
+        speed: 300,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+        ]
+    });
+});
