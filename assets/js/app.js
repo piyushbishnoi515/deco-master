dca9fc498ffc9bed577be5021b22bfb531a2336f
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-07-08 09:59:06 +0530

    section-2 complete with responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         dots: false,
         infinite: true,
         speed: 300,
-        autoplay: true,
+        autoplay: false,
         slidesToShow: 4,
         slidesToScroll: 2,
         arrows: false,
